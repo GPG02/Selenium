0405fd67b904ac8156d9967ec0add31f3e686bfa
AksharaBhanu <javagalbhanu@gmail.com>
2023-12-07 09:04:30 +0530

    'Day4'

---
+++ B93/src/day4/Demo2.java
@@ -0,0 +1,20 @@
+package day4;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("file:///F:/B93%20Selenium/Workspace/pages/sample2.html");
+		
+		WebElement e = driver.findElement(By.tagName("x"));
+		
+		e.click();
+	}
+
+}
